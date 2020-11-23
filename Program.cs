5e84ce67fcd52bd7ed007815c1f5c04287829641
krugersu <pk69@kruger.su>
2020-11-23 17:05:04 +0700

    23.11.20

--- Program.cs
+++ Program.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.Linq;
+using System.Data.SqlClient;
 
 namespace StockBalances
 {
     class Program
     {
         static void Main(string[] args)
+
         {
+            //    DataJson n = new DataJson();
+            //string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=usersdb;Integrated Security=True";
+            // получаем строку подключения
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            Console.WriteLine(connectionString);
+
+            // Создание подключения
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                // Открываем подключение
+                connection.Open();
+                Console.WriteLine("Подключение открыто");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // закрываем подключение
+                connection.Close();
+                Console.WriteLine("Подключение закрыто...");
+            }
+
+            Console.Read();
         }
     }
 }
